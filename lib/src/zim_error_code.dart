a52331df1f19fc60cb770ddd50fc6fe3b8d0c926
ervinchen <ervinchen@zego.im>
2023-10-10 17:04:40 +0800

    ✨ Update error code with group dismissed

--- lib/src/zim_error_code.dart
+++ lib/src/zim_error_code.dart
@@ -98,6 +98,7 @@ class ZIMErrorCode {
   static const int groupModuleGroupAlreadyExists = 6000524;
   static const int groupModuleGroupMemberHasReachedLimit = 6000525;
   static const int groupModuleGroupAttributeDoseNotExist = 6000526;
+  static const int groupModuleGroupWithDismissed = 6000527;
   static const int groupModuleTheNumberOfGroupAttributesExceedsLimit = 6000531;
   static const int groupModuleTheLengthOfGroupAttributeKeyExceedsLimit =
       6000532;
