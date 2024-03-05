b6d05e959a735c8172832e448a8a616c3b051149
cc <qingweiping@zego.im>
2024-03-05 20:39:54 +0800

    🐛 Fix build error

--- windows/internal/ZIMPluginConverter.cpp
+++ windows/internal/ZIMPluginConverter.cpp
@@ -1551,10 +1551,12 @@ FTMap ZIMPluginConverter::cnvZIMFriendApplicationInfoToMap(const ZIMFriendApplic
 	return infoMap;
 }
 
-FTMap ZIMPluginConverter::cnvZIMGroupMemberSimpleInfoToMap(const ZIMGroupMemberSimpleInfo& info) {
+
+
+FTMap ZIMPluginConverter::cnvZIMGroupMemberSimpleInfoToMap(std::shared_ptr<ZIMGroupMemberSimpleInfo> operatedUser) {
 	FTMap infoMap;
-	infoMap[FTValue("memberNickname")] = FTValue(info.memberNickname);
-	infoMap[FTValue("memberRole")] = FTValue((int32_t)info.memberRole);
+	infoMap[FTValue("memberNickname")] = FTValue(operatedUser->memberNickname);
+	infoMap[FTValue("memberRole")] = FTValue((int32_t)operatedUser->memberRole);
 	return infoMap;
 }
 
